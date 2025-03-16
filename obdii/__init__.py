f90c5d314713e9ebe8de147962e635954822c8d1
PaulMarisOUMary <paul.marisoumary@gmail.com>
2025-03-16 01:17:56 +0100

    fix missing protocols import

--- obdii/__init__.py
+++ obdii/__init__.py
@@ -2,13 +2,16 @@ __title__ = "obdii"
 __author__ = "PaulMarisOUMary"
 __license__ = "MIT"
 __copyright__ = "Copyright 2025-present PaulMarisOUMary"
-__version__ = "0.1.0a3"
+__version__ = "0.1.0a4"
 
 from logging import NullHandler, getLogger
 
 from .connection import Connection
 from .commands import commands
 from .modes.modeat import at_commands
+
+# We must __init__ .protocols to BaseProtocol.register supported protocols
+from .protocols import *
 
 __all__ = [
     "Connection",
