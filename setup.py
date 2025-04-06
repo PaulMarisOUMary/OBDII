34ed404eb44a0dd0d8ddbb0193d6a5fd4e7c8d78
PaulMarisOUMary <paul.marisoumary@gmail.com>
2025-04-06 02:08:11 +0200

    remove env based condition for local_scheme

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
+from os import environ
 from setuptools import setup
 
-setup()
+ci_environment = environ.get("CI", "false").lower() == "true"
+
+local_scheme = "no-local-version" if ci_environment else "node-and-date"
+
+setup(
+    use_scm_version={
+        "local_scheme": local_scheme,
+    }
+)
