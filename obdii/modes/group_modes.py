8a7350ab96b9e4e977125f39be27b5dbf0668dff
PaulMarisOUMary <paul.marisoumary@gmail.com>
2025-12-28 14:35:41 +0100

    Add exact overloads for GroupModes __getitem__
    - Ugly, however, narrows down the exact GroupCommands for typehinting

--- obdii/modes/group_modes.py
+++ obdii/modes/group_modes.py
@@ -1,4 +1,6 @@
-from typing import Union, overload
+from typing import Union, Literal, overload
+
+from . import Mode01, Mode02, Mode03, Mode04, Mode09
 
 from .basetypes import Modes, ModesType
 from .group_commands import GroupCommands
@@ -18,9 +20,29 @@ class GroupModes(Modes):
     def __getitem__(self, key: str) -> Command: ...
 
     @overload
-    def __getitem__(self, key: Union[Mode, int]) -> ModesType: ...
+    def __getitem__(self, key: Union[Literal[Mode.REQUEST], Literal[1]]) -> Mode01: ...
 
-    def __getitem__(self, key: Union[Mode, int, str]):
+    @overload
+    def __getitem__(
+        self, key: Union[Literal[Mode.FREEZE_FRAME], Literal[2]]
+    ) -> Mode02: ...
+
+    @overload
+    def __getitem__(
+        self, key: Union[Literal[Mode.STATUS_DTC], Literal[3]]
+    ) -> Mode03: ...
+
+    @overload
+    def __getitem__(
+        self, key: Union[Literal[Mode.CLEAR_DTC], Literal[4]]
+    ) -> Mode04: ...
+
+    @overload
+    def __getitem__(
+        self, key: Union[Literal[Mode.VEHICLE_INFO], Literal[9]]
+    ) -> Mode09: ...
+
+    def __getitem__(self, key: Union[Mode, int, str]) -> Union[ModesType, Command]:  # type: ignore[invalid-method-override]
         if not isinstance(key, (Mode, int)):
             return super().__getitem__(key)
 
