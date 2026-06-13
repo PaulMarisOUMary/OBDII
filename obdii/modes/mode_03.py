6496cb0e86c8a899d89380c681663a891e7facdc
PaulMarisOUMary <paul.marisoumary@gmail.com>
2026-06-13 15:06:25 +0200

    Allow every command to have a generic typed response.value bound
    - Remove partial which was previously blocking type inference

--- obdii/modes/mode_03.py
+++ obdii/modes/mode_03.py
@@ -1,5 +1,3 @@
-from functools import partial
-
 from .group_commands import GroupCommands
 
 from ..command import Command
@@ -8,7 +6,7 @@ from ..parsers.dtc import DTC
 
 
 M = Mode.STATUS_DTC
-C = partial(Command, M)
+C = Command
 
 # https://en.wikipedia.org/wiki/OBD-II_PIDs#Service_03_-_Show_stored_Diagnostic_Trouble_Codes_(DTCs)
 
@@ -16,5 +14,5 @@ C = partial(Command, M)
 class Mode03(GroupCommands, registry_id=M):
     """Get Diagnostic Trouble Codes Command"""
 
-    GET_DTC = C('', resolver=DTC.parse)
+    GET_DTC = C(M, '', resolver=DTC.parse)
     """Request Diagnostic Trouble Codes (DTCs)"""
--- obdii/modes/mode_04.py
+++ obdii/modes/mode_04.py
@@ -1,5 +1,3 @@
-from functools import partial
-
 from .group_commands import GroupCommands
 
 from ..command import Command
@@ -7,12 +5,12 @@ from ..mode import Mode
 
 
 M = Mode.CLEAR_DTC
-C = partial(Command, M)
+C = Command
 
 # https://en.wikipedia.org/wiki/OBD-II_PIDs#Service_04_-_Clear_Diagnostic_Trouble_Codes_and_stored_values
 
 class Mode04(GroupCommands, registry_id=M):
     """Clear Diagnostic Trouble Codes Command"""
 
-    CLEAR_DTC = C('')
+    CLEAR_DTC = C(M, '')
     """Clear Diagnostic Trouble Codes / Malfunction indicator lamp (MIL) / Check engine light and Freeze frame data"""
